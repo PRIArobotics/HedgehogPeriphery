d44a766fd7ccd687a995caf30fe4f43cda540092
SillyFreak <sillyfreak.ck@gmail.com>
2016-09-25 18:21:46 +0200

    put raspberry dependencies into extra; these can't be installed in Windows

--- setup.py
+++ setup.py
@@ -46,12 +46,12 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['RPi.GPIO', 'pyserial'],
+    install_requires=[],
 
     # You can install these using the following syntax, for example:
     # $ pip install -e .[dev,test]
     extras_require={
-        # 'dev': ['invoke'],
+        'raspberry': ['RPi.GPIO', 'pyserial'],
     },
 
     # package_data={
